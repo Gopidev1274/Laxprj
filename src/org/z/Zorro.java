a7e64ff7f2d9dc656ff304beae9f03ac05d65936
Gopidev1274 <gopigamer.1247@gmail.com>
2019-09-11 05:47:52 +0530

    Laxprj

---
+++ src/org/z/Zorro.java
@@ -0,0 +1,14 @@
+package org.z;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zorro {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\GopiDeva\\LAX\\driver\\chromedriver.exe");
+	WebDriver dev=new ChromeDriver();
+	dev.get("https://www.youtube.com");
+	dev.manage().window().maximize();
+	dev.quit();
+}
+}
