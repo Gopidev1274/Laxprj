ae9b862ef4ff7def8a7fa67fdadd90994961405f
Gopidev1274 <gopigamer.1247@gmail.com>
2019-09-13 03:36:57 +0530

    devprj

--- src/org/z/Zorro.java
+++ src/org/z/Zorro.java
@@ -1,14 +1,25 @@
 package org.z;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Zorro {
-public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "C:\\GopiDeva\\LAX\\driver\\chromedriver.exe");
-	WebDriver dev=new ChromeDriver();
-	dev.get("https://www.youtube.com");
-	dev.manage().window().maximize();
-	dev.quit();
-}
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\GopiDeva\\LAX\\driver\\chromedriver.exe");
+		WebDriver dev = new ChromeDriver();
+		dev.get("https://www.youtube.com");
+		TakesScreenshot tk = (TakesScreenshot) dev;
+		File dev1 = tk.getScreenshotAs(OutputType.FILE);
+		System.out.println(dev1);
+		File dev2 = new File("C:\\Users\\ELCOT\\Desktop\\Screeenshot\\dev2.jpg");
+		FileUtils.copyFile(dev1, dev2);
+		dev.manage().window().maximize();
+		dev.quit();
+	}
 }
---
+++ src/org/zet/SetPrg.java
@@ -0,0 +1,15 @@
+package org.zet;
+
+import java.util.*;
+
+public class SetPrg {
+public static void main(String[] args) {
+Set<Integer> li=new LinkedHashSet();
+li.add(9);
+li.add(8);
+li.add(6);
+System.out.println(li);
+System.out.println("LOCATORS:id,name,classname,xpath,tagname,linktext,partiallylinktext,cssselector");
+
+}
+}
